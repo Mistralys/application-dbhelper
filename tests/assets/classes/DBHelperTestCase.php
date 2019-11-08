26a1cfdefa53cf287c40cbadd23f3efc203c204a
Mistralys <s.mordziol@gmail.com>
2019-11-08 13:03:57 +0100

    Added more utility methods, and more test data.

--- tests/assets/classes/DBHelperTestCase.php
+++ tests/assets/classes/DBHelperTestCase.php
@@ -7,6 +7,13 @@ use AppDB\DBHelper_Exception;
 
 abstract class DBHelper_TestCase extends TestCase
 {
+   /**
+    * Configures the test database connection using the configuration
+    * settings as specified in the bootstrap.php tests file.
+    * 
+    * @throws DBHelper_Exception
+    * @return DBHelper_Database
+    */
     protected function configureTestDatabase() : DBHelper_Database
     {
         $db = DBHelper::addDatabase('tests', TESTS_DB_NAME)
@@ -27,16 +34,46 @@ abstract class DBHelper_TestCase extends TestCase
         
         return $db;
     }
+
+   /**
+    * Fetches any of the test product IDs from the database.
+    * Must have called the insertTestData() method for this
+    * to work.
+    * 
+    * @return int
+    */
+    protected function getRecordID() : int
+    {
+        $id = DBHelper::fetchKey('product_id', "SELECT product_id FROM products");
+        if($id !== null) {
+            return (int)$id;
+        }
+        
+        throw new Exception('Cannot get test record ID: no test records have been inserted.');
+    }
     
     protected $testProducts = array(
-        array('label' => 'Product one', 'price' => '100'),
-        array('label' => 'Product two', 'price' => '14.99'),
-        array('label' => 'Product three', 'price' => '60'),
-        array('label' => 'Product foo with bar', 'price' => '1000'),
+        array('label' => 'Product one', 'price' => '100', 'in_stock' => 'yes', 'date_added' => '2019-06-01 12:00:00'),
+        array('label' => 'Product two', 'price' => '14.99', 'in_stock' => 'no'),
+        array('label' => 'Product three', 'price' => '60', 'in_stock' => 'no'),
+        array('label' => 'Product foo with bar', 'price' => '1000', 'in_stock' => 'yes'),
     );
     
+   /**
+    * Resets the DBHelper and inserts all products from the
+    * test products array into the database. Call clearTestData()
+    * afterwards to remove them again.
+    * 
+    * @see DBHelper_TestCase::clearTestData()
+    */
     protected function insertTestData()
     {
+        \AppLocalize\Localization::selectAppLocale('en_UK');
+        
+        DBHelper::reset();
+        
+        $this->configureTestDatabase();
+        
         DBHelper::startTransaction();
         
         DBHelper::truncate('products');
@@ -48,10 +85,33 @@ abstract class DBHelper_TestCase extends TestCase
         DBHelper::commitTransaction();
     }
     
+   /**
+    * Removes all test products from the database. Has no 
+    * effect if they have already been cleared.
+    */
     protected function clearTestData()
     {
         DBHelper::startTransaction();
         DBHelper::truncate('products');
         DBHelper::commitTransaction();
+    }
+
+   /**
+    * Creates an instance of the test base collection.
+    * 
+    * @return TestBaseCollection
+    */
+    protected function createCollection() : TestBaseCollection
+    {
+        return DBHelper::createCollection(TestBaseCollection::class);
+    }
+    
+    protected function getTestRecord() : TestBaseRecord
+    {
+        $id = $this->getRecordID();
+        
+        $collection = $this->createCollection();
+        
+        return $collection->getByID($id);
     }
 }
