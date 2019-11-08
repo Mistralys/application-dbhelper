02b061decf69dd746397fec64779d0d2566af7c2
Mistralys <s.mordziol@gmail.com>
2019-11-08 13:04:17 +0100

    Added some methods.

--- tests/assets/classes/TestBaseRecord.php
+++ tests/assets/classes/TestBaseRecord.php
@@ -2,9 +2,32 @@
 
 class TestBaseRecord extends \AppDB\DBHelper_BaseRecord
 {
+    protected $onCreatedCalled = false;
+    
     protected function recordRegisteredKeyModified($name, $label, $isStructural, $oldValue, $newValue)
     {
         
+    }
+    
+    public function setLabel(string $label) : TestBaseRecord
+    {
+        $this->setRecordKey('label', $label);
+        return $this;
+    }
+    
+    public function getLabel() : string
+    {
+        return $this->getRecordStringKey('label');
+    }
+    
+    public function onCreated()
+    {
+        $this->onCreatedCalled = true;
+    }
+
+    public function hasOnCreatedBeenCalled() : bool
+    {
+        return $this->onCreatedCalled;
     }
 }
     
