155f2412b1c28658806c29e79174f59cd55fff42
Mistralys <s.mordziol@gmail.com>
2019-11-08 13:00:16 +0100

    Added more columns for testing different types.

--- tests/assets/sql/tests.sql
+++ tests/assets/sql/tests.sql
@@ -5,7 +5,9 @@
 CREATE TABLE `products` (
   `product_id` int(11) UNSIGNED NOT NULL,
   `label` varchar(250) NOT NULL,
-  `price` decimal(10,0) NOT NULL
+  `price` decimal(10,0) NOT NULL,
+  `date_added` datetime NOT NULL DEFAULT current_timestamp(),
+  `in_stock` enum('yes','no') NOT NULL DEFAULT 'yes'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -16,7 +18,9 @@ CREATE TABLE `products` (
 -- Indexes for table `products`
 --
 ALTER TABLE `products`
-  ADD PRIMARY KEY (`product_id`);
+  ADD PRIMARY KEY (`product_id`),
+  ADD KEY `in_stock` (`in_stock`),
+  ADD KEY `date_added` (`date_added`);
 
 --
 -- AUTO_INCREMENT for dumped tables
